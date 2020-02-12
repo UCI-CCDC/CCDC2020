c0e5a380bf261eb7c55ced1cd920d79d90620c59
Jacob <20913473+PwnKitteh@users.noreply.github.com>
2020-02-11 19:02:32 -0800

    small fixes
    I fucked up my commits again, goddammit

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 Tools and info for CCDC 2020 competition
 
 
+NOTE: snort-install.sh is kind of broken at the moment
 shortened links to raw- 
  * snort-install.sh - https://git.io/JvtRS
  * inventory.sh - https://git.io/Jvt0g
 
  getting and running scripts:
- `sudo wget [link] -O inventory.sh && sudo bash inventory.sh`
+ `sudo wget [link] -O inv.sh && sudo bash inv.sh`
--- inventory.sh
+++ inventory.sh
@@ -12,6 +12,21 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+#this is for accepting flags to perform different operations
+#if a flag is supposed to accept user input after being called (ex -f "hello"), it is followed by a : after getopts in the while statement
+while getopts u:d:p:f: option
+do
+case "${option}"
+in
+u) USER=${OPTARG};;
+d) DATE=${OPTARG};;
+p) PRODUCT=${OPTARG};;
+f) FORMAT=${OPTARG};;
+esac
+done
+
+
+
 printf "\n*** generating audit.txt in your home directory\n"
 touch ~/audit.txt 
 adtfile="tee -a $HOME/audit.txt"
@@ -21,9 +36,31 @@ adtfile="tee -a $HOME/audit.txt"
 
 
 cat /etc/os-release | grep -w "PRETTY_NAME" | cut -d "=" -f 2 | $adtfile
+osOut='cat /etc/os-release | grep -w "NAME" | cut -d "=" -f 2 '
+echo $osOut | $adtfile
 
 
 if [ "$osOut" == "Alpine" ] ; then
+    alpinelp=1
+    while [ "$alpinelp" == 1 ] ; do
+        printf "Alpine? lol k, do you want to install some basic stuff? [y/N/? for list]"
+        read -r alpinechoice
+            case "$alpinechoice" in 
+            Y|y) apk update && apk upgrade && apk install bash vim curl man man-pages mdocml-apropos bash-doc bash-completion util-linux pciutils usbutils coreutils binutils findutils 
+            alpinelp=0;;
+            N|n) alpinelp=0;; 
+            w) printf "bash vim curl man man-pages mdocml-apropos bash-doc bash-completion util-linux pciutils usbutils coreutils binutils findutils";;
+            *) printf "invalid choice" 
+        esac
+    done
+fi
+
+
+
+
+if [ "$osOut" == "Alpine" ] ; then
+#alpine linux will not be at regionals
+if [ "$osOut" == "Alpine Linux" ] ; then
     alpinelp=1
     while [ "$alpinelp" == 1 ] ; do
         printf "Alpine? lol k, do you want to install some basic stuff? [y/N/? for list]"
@@ -98,3 +135,22 @@ printf '\n\n**services you should cry about***\n'
 ps aux | grep 'Docker\|samba\|postfix\|dovecot\|smtp\|psql\|ssh\|clamav\|mysql' | grep -v "grep"
 
 
+printf '**services you should cry about***\n'
+ps aux | grep 'Docker\|samba\|postfix\|dovecot\|smtp\|psql\|ssh\|clamav\|mysql\|bind9'
+
+
+## NOTE WORKING O NTHIS FOR NOW, IDK IF THERE IS ALWAYS A .BASH_PROFILE IN ~
+echo 'NOTE THIS MIGHT NOT WORK'
+ # shellcheck disable=SC2016
+printf '*** Making Bash profile log time/date using at $HOME/.bash_profile ***'
+ # shellcheck disable=SC2183
+if printf 'export HISTTIMEFORMAT="%d/%m/%y %T"' >> ~/.bash_profile >/dev/null 2>/dev/null == 0 ; then 
+    # shellcheck source=/dev/null
+      source ~/.bash_profile
+    
+else 
+    echo something went wrong with making bash profile track time! 
+fi
+
+#curl -
+#pull the external audit.sh script
