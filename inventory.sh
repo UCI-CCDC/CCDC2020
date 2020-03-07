5acbe45262e0a25c28b6beb520b87b278cb19693
Jacob <20913473+PwnKitteh@users.noreply.github.com>
2020-03-07 14:21:32 -0800

    another edit to --database in the mysql restore

--- inventory.sh
+++ inventory.sh
@@ -151,7 +151,7 @@ r)
 
     tar -xzf "$OPTARG" -C restore-sql/
     for db in $(find restore-sql/ -name *.sql); do
-        mysql -u root -p$pass --database=${db%.sql} < "$db"
+        mysql -u root -p$pass --database=${(basename $db)%.sql} < "$db"
     done
 
     exit 1;;
